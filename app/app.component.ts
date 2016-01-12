81184e3a0725446e16cbde8b3a174c60beb7492d
DerrickJames <dmushangi@gmail.com>
2016-01-12 19:57:08 +0300

    Adding two way data binding on ninja name input element

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,28 @@
 import {Component} from 'angular2/core';
 
+interface Ninja {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+        <h1>{{title}}</h1> 
+        <h2>{{ninja.name}} details!</h2>
+        <div><label>ID: </label>{{ninja.id}}</div>
+        <div>
+            <label>NAME: </label>
+            <div><input [(ngModel)]="ninja.name" placeholder="Name"></div>
+        </div>
+    `
 })
 
-export class AppComponent { }
+export class AppComponent {
+    public title = 'Ninjas Wall Of Fame';
+
+    public ninja: Ninja = {
+        id: 1,
+        name: 'Ricky James'
+    };
+}
