bfcea2171f9ba010e8b438a92307994656504e18
DerrickJames <dmushangi@gmail.com>
2016-01-20 12:11:31 +0300

    Importing "InitCapsPipe" and putting it to use

--- src/app/ninja-detail.component.ts
+++ src/app/ninja-detail.component.ts
@@ -1,19 +1,21 @@
 import {Component} from 'angular2/core';
 import {Ninja} from './ninja';
+import {InitCapsPipe} from './init-caps-pipe';
 
 @Component({
     selector: 'ninja-detail',
     inputs: ['ninja'],
     template: `
         <div *ngIf="ninja">
-            <h2>{{ninja.name}} details!</h2>
+            <h2>{{ninja.name | initCaps}} details!</h2>
             <div><label>ID: </label>{{ninja.id}}</div>
             <div>
                 <label>NAME: </label>
                 <div><input [(ngModel)]="ninja.name" placeholder="Name"></div>
             </div>
         </div>
-    `
+    `,
+    pipes: [InitCapsPipe]
 })
 
 export class NinjaDetailComponent {
