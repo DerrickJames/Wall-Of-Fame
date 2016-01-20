bd970bc4b3b618e42f520efe6c555851b9dc7491
DerrickJames <dmushangi@gmail.com>
2016-01-20 12:09:31 +0300

    Reformat name property to lowercase for testing
    Reformat the name property in the Ninja object to begin with a small letter for
    testing the functionality of the custom pipe.

--- src/app/mock-ninjas.ts
+++ src/app/mock-ninjas.ts
@@ -1,14 +1,14 @@
 import {Ninja} from './ninja';
 
 export var NINJAS: Ninja[] = [
-    { "id": 1, "name": "Derrick James" },
-    { "id": 2, "name": "Athman Gude" },
-    { "id": 3, "name": "Ronny Nyaga" },
-    { "id": 4, "name": "Morris Maina" },
-    { "id": 5, "name": "James Mshai" },
-    { "id": 6, "name": "Steve Kiriba" },
-    { "id": 7, "name": "Steve Murathe" },
-    { "id": 8, "name": "Mary Nunga" },
-    { "id": 9, "name": "Leone Munene" },
-    { "id": 10, "name": "Lavender Naomi" }
+    { "id": 1, "name": "derrick james" },
+    { "id": 2, "name": "athman gude" },
+    { "id": 3, "name": "ronnie nyaga" },
+    { "id": 4, "name": "morris maina" },
+    { "id": 5, "name": "james mshai" },
+    { "id": 6, "name": "steve kiriba" },
+    { "id": 7, "name": "steve murathe" },
+    { "id": 8, "name": "mary munga" },
+    { "id": 9, "name": "leone munene" },
+    { "id": 10, "name": "lavender naomi" }
 ];
