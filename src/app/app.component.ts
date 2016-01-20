099dd4bb203ecc853516a37bae7d65364f0cc850
DerrickJames <dmushangi@gmail.com>
2016-01-20 09:41:38 +0300

    Modify the file structure

---
+++ src/app/app.component.ts
@@ -0,0 +1,102 @@
+import {Component} from 'angular2/core';
+import {OnInit} from 'angular2/core';
+
+import {Ninja} from './ninja';
+import {NinjaService} from './ninja.service';
+import {NinjaDetailComponent} from './ninja-detail.component';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1>{{title}}</h1> 
+        <h2>Ninjas List</h2>
+        <ul class="list-items">
+            <li *ngFor="#ninja of ninjas" 
+                (click)="onSelect(ninja)"
+                [class.selected]="ninja === selectedNinja">
+
+                <span class="badge">{{ninja.id}}</span> {{ninja.name}}
+            </li>
+        </ul>
+
+        <ninja-detail [ninja]="selectedNinja"></ninja-detail>
+    `,
+
+    styles: [`
+        .selected {
+            background-color: #CFD8DC !important;
+            color: white;
+        }
+
+        .list-items {
+            margin: 0 0 2em 0;
+            list-style-type: none;
+            padding: 0;
+            width: 15em;
+        }
+
+        .list-items li {
+            cursor: pointer;
+            position: relative;
+            left: 0;
+            background-color: #EEE;
+            margin: .5em;
+            padding: .3em 0em;
+            height: 1.6em;
+            border-radius: 4px;
+        }
+
+        .list-items li.selected:hover {
+            color: white;
+        }
+
+        .list-items li:hover {
+            color: #607D8B;
+            background-color: #EEE;
+            left: .1em;
+        }
+
+        .list-items .text {
+            position: relative;
+            top: -3px;
+        }
+
+        .list-items .badge {
+            display: inline-block;
+            font-size: small;
+            color: white;
+            padding: 0.8em 0.7em 0em 0.7em;
+            background-color: #607D8B;
+            line-height: 1em;
+            position: relative;
+            left: -1px;
+            top: -4px;
+            height: 1.8em;
+            margin-right: .8em;
+            border-radius: 4px 0px 0px 4px;
+        }
+    `],
+
+    directives: [NinjaDetailComponent],
+    providers: [NinjaService]
+})
+
+export class AppComponent implements OnInit {
+    public title  = 'Ninjas Wall Of Fame';
+    public ninjas: Ninja[];
+
+    public selectedNinja: Ninja;
+
+    constructor(private _ninjaService: NinjaService) { }
+
+    ngOnInit() {
+        this.getNinjas();
+    }
+
+    onSelect(ninja: Ninja) { this.selectedNinja = ninja; }
+
+    getNinjas() {
+        this._ninjaService.getNinjas().then(ninjas => this.ninjas = ninjas);
+    }
+}
+
---
+++ src/app/mock-ninjas.ts
@@ -0,0 +1,14 @@
+import {Ninja} from './ninja';
+
+export var NINJAS: Ninja[] = [
+    { "id": 1, "name": "Derrick James" },
+    { "id": 2, "name": "Athman Gude" },
+    { "id": 3, "name": "Ronny Nyaga" },
+    { "id": 4, "name": "Morris Maina" },
+    { "id": 5, "name": "James Mshai" },
+    { "id": 6, "name": "Steve Kiriba" },
+    { "id": 7, "name": "Steve Murathe" },
+    { "id": 8, "name": "Mary Nunga" },
+    { "id": 9, "name": "Leone Munene" },
+    { "id": 10, "name": "Lavender Naomi" }
+];
