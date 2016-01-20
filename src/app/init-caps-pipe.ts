fc5db00b0fa49c77cba6edc64f237180ff164d1f
DerrickJames <dmushangi@gmail.com>
2016-01-20 11:35:25 +0300

    Add a custom "InitCapsPipe" file

---
+++ src/app/init-caps-pipe.ts
@@ -0,0 +1,10 @@
+import {Pipe} from 'angular2/core';
+
+@Pipe({ name: 'initCaps' })
+export class InitCapsPipe {
+    transform(value: string) {
+        return value.toLowerCase().replace(/(?:^|\s)[a-z]/g, function(m) {
+            return m.toUpperCase();
+        });
+    }
+}
