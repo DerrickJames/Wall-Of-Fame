ec61510305987356dd6abcfd49f49582495712de
DerrickJames <dmushangi@gmail.com>
2016-01-29 12:15:53 +0300

    Update ninjas component to point to styles and mark-up
    Make necessary changes to the ninjas component to point
    to its mark-up template and stylesheet.

--- src/app/ninjas.component.ts
+++ src/app/ninjas.component.ts
@@ -8,75 +8,8 @@ import {NinjaDetailComponent} from './ninja-detail.component';
 
 @Component({
     selector: 'my-ninjas',
-    template: `
-        <h2>Ninjas List</h2>
-        <ul class="list-items">
-            <li *ngFor="#ninja of ninjas" 
-                (click)="onSelect(ninja)"
-                [class.selected]="ninja === selectedNinja">
-
-                <span class="badge">{{ninja.id}}</span> {{ninja.name | initCaps}}
-            </li>
-        </ul>
-
-        <ninja-detail [ninja]="selectedNinja"></ninja-detail>
-    `,
-
-    styles: [`
-        .selected {
-            background-color: #CFD8DC !important;
-            color: white;
-        }
-
-        .list-items {
-            margin: 0 0 2em 0;
-            list-style-type: none;
-            padding: 0;
-            width: 15em;
-        }
-
-        .list-items li {
-            cursor: pointer;
-            position: relative;
-            left: 0;
-            background-color: #EEE;
-            margin: .5em;
-            padding: .3em 0em;
-            height: 1.6em;
-            border-radius: 4px;
-        }
-
-        .list-items li.selected:hover {
-            color: white;
-        }
-
-        .list-items li:hover {
-            color: #607D8B;
-            background-color: #EEE;
-            left: .1em;
-        }
-
-        .list-items .text {
-            position: relative;
-            top: -3px;
-        }
-
-        .list-items .badge {
-            display: inline-block;
-            font-size: small;
-            color: white;
-            padding: 0.8em 0.7em 0em 0.7em;
-            background-color: #607D8B;
-            line-height: 1em;
-            position: relative;
-            left: -1px;
-            top: -4px;
-            height: 1.8em;
-            margin-right: .8em;
-            border-radius: 4px 0px 0px 4px;
-        }
-    `],
-
+    templateUrl: 'src/app/ninjas.component.html',
+    styleUrls: ['src/app/ninjas.component.css'],
     directives: [NinjaDetailComponent],
     pipes: [InitCapsPipe]
 })
