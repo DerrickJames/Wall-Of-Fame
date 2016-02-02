7271b90033bfe324924b108dc8526b5be2fdcb9c
DerrickJames <dmushangi@gmail.com>
2016-02-02 10:10:06 +0300

    Isolate ninjas routes by ninjas feature module
    Export ninjas specific routes as a variable for modularity.

---
+++ src/app/ninjas/ninjas.routes.ts
@@ -0,0 +1,10 @@
+import {NinjasListComponent} from './ninjas-list.component';
+
+export var NINJASROUTES = [
+    { 
+        path: '/',
+        name: 'Ninjas',
+        component: NinjasListComponent,
+        useAsDefault: true
+    }
+];
