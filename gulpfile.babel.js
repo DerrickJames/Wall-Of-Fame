ec04fdee07b79e4cec0a847de7f7f2b501b7d0d1
DerrickJames <dmushangi@gmail.com>
2016-02-03 21:44:39 +0300

    Import gulp-load-plugins
    Add and initialize gulp to autoload plugins.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,10 +2,22 @@
 
 import gulp from 'gulp';
 import del from 'del';
-import typescript from 'gulp-typescript';
 import tscConfig from './tsconfig';
-import util from 'gulp-util';
+import gulpPlugins from 'gulp-load-plugins';
+
+const plugins = gulpPlugins({lazy: true});
+
+gulp.task('help', plugins.taskListing);
+gulp.task('default', ['help']);
+
+gulp.task('vet', () => {
+    plugins.util.log(plugins.util.colors.green('Analyzing source with TSLint!'));
+
+    //return gulp.src('src/app/**/*.ts')
+    //    .pipe(plugins.tslint.tslint())
+    //    .pipe(plugins.tslint.report('verbose'));
+});
 
 gulp.task('clean', () => {
-   util.log('Cleaning files somehow');
+   plugins.util.log(plugins.util.colors.green('Cleaning files somehow'));
 });
