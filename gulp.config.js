76884df83fe1b4c2ef24d230da7ce9cbf5b3aa03
DerrickJames <dmushangi@gmail.com>
2016-02-04 22:18:18 +0300

    Define base directory and stylesheet paths

--- gulp.config.js
+++ gulp.config.js
@@ -1,4 +1,9 @@
 export const ROOT = './';
-export const ROOT_SRC = ROOT + 'src/';
-export const APP_SRC = ROOT_SRC + 'app/';
-export const TEMP = ROOT + '.tmp';
+export const ROOT_SRC = `${ROOT}src/`;
+export const APP_SRC = `${ROOT_SRC}app/`;
+export const TEMP = `${ROOT}.tmp/`;
+
+export const CONFIG = {
+    less: `${ROOT_SRC}styles/styles.less`,
+    tempCss: `${TEMP}styles/`
+};
