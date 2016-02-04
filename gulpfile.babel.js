3ab349474802a5ec55a7d3ee653b6f42de657e7a
DerrickJames <dmushangi@gmail.com>
2016-02-04 22:18:59 +0300

    Define a styles task to compile LESS to CSS

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,7 @@ import gulp from 'gulp';
 import del from 'del';
 import tscConfig from './tsconfig';
 import gulpPlugins from 'gulp-load-plugins';
+import {CONFIG} from './gulp.config';
 
 const plugins = gulpPlugins({lazy: true});
 
@@ -18,6 +19,18 @@ gulp.task('vet', () => {
     //    .pipe(plugins.tslint.report('verbose'));
 });
 
+gulp.task('styles', () => {
+    plugins.util.log('Compiling: ' + plugins.util.colors.green('LESS ---> CSS'));
+
+   return gulp
+        .src(CONFIG.less)
+        .pipe(plugins.plumber())
+        .pipe(plugins.less())
+        .pipe(plugins.autoprefixer({browser: ['last 2 version', '> 5%']}))
+        .pipe(gulp.dest(CONFIG.tempCss));
+});
+
 gulp.task('clean', () => {
    plugins.util.log(plugins.util.colors.green('Cleaning files somehow'));
+
 });
